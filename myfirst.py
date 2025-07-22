032f2d5e586b18e90735ce18c9342a137be36b18
MD WALIULLAH <122639359+Mdwali79986@users.noreply.github.com>
2025-07-22 12:42:46 +0530

    Add files via upload

---
+++ myfirst.py
@@ -0,0 +1,23 @@
+
+print("Hello, World!")
+print("This is a basic Python script.")
+
+name = "Wali"
+age = 21
+city = "Ranchi"
+
+
+print("\n--- User Info ---")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+
+a = 10
+b = 5
+sum_result = a + b
+print("\nSum of", a, "and", b, "is:", sum_result)
+
+
+user_input = input("\nEnter your favorite color: ")
+print("Wow! I like", user_input, "too.")
